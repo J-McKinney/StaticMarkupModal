18911ad09de60c79dac4be8d2f542397ccd6d4c9
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-01 16:42:53 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MyModal from "./MyModal/MyModal";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        <MyModal />
       </header>
     </div>
   );
